d8091410c4dcf4329743b1c25e820e43f381c382
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-22 19:54:00 -0300

    feat: implement chat streaming messages

--- chat_client.py
+++ chat_client.py
@@ -3,11 +3,28 @@ import chat_pb2
 import time
 import grpc
 
+def chat():
+    while True:
+        message = input("Please enter a message (or nothing to stop chatting): ")
+
+        if message == "":
+            break
+
+        msg_request = chat_pb2.ChatMessageRequest(name = "You", message = message)
+        yield msg_request
+        time.sleep(1)
+
 def run():
     with grpc.insecure_channel('localhost:5000') as channel:
-        chat_grpc.ChatServiceStub(channel)
+        stub = chat_grpc.ChatServiceStub(channel)
         
+        responses = stub.BidiChat(chat())
+
+        for response in responses:
+            print("Interacting response received: ")
+            print(response)
+
 
 if __name__ == '__main__':
-    print('Starting bidirectional chat between client [stub] and server')
+    print('Starting bidirectional chat between client and server')
     run()
