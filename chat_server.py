206ccf337373f1569f5d11076f31e0414972dfa2
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-22 19:32:48 -0300

    feat: add constructor for server

---
+++ chat_server.py
@@ -0,0 +1,10 @@
+from concurrent import futures
+import time
+
+import grpc
+import chat_pb2
+import chat_pb2_grpc as chat_grpc
+
+class ChatServicer(chat_grpc.ChatServiceServicer):
+    def Chat(self, request, context):
+        return super().BidiChat(request, context);
