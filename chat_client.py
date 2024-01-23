5fcf4ce369f9be23a27f7031a73fd038aea9f5e0
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-01-23 09:38:48 -0300

    style: format code

--- chat_client.py
+++ chat_client.py
@@ -4,8 +4,9 @@ import chat_pb2_grpc
 from concurrent import futures
 import threading
 
+
 class ChatClient:
-    def __init__(self, name:str, host:str, port:str):
+    def __init__(self, name: str, host: str, port: str):
         self.name = name
         self.channel = grpc.insecure_channel("{}:{}".format(host, port))
         self.stub = chat_pb2_grpc.ChatServiceStub(self.channel)
@@ -16,17 +17,19 @@ class ChatClient:
         for response in response_iterator:
             print(f"[{response.name}]: {response.msg}")
 
+
 def receive_messages(client):
     message_response = chat_pb2.ChatMessageRequest(name=client.name, msg="")
     response_iterator = client.stub.BidiChat(iter([message_response]))
     for response in response_iterator:
         print(f"[{response.name}]: {response.msg}")
 
+
 if __name__ == "__main__":
     name = input("Enter your name: ")
     host = 'localhost'
     port = '50051'
-    client = ChatClient(name = name, host = host, port= port)
+    client = ChatClient(name=name, host=host, port=port)
 
     try:
         while True:
